6540dfc91231e5b3f529a0b4072e404b129adb24
mcodemax <mwalterjohnson7@gmail.com>
2021-06-28 07:04:51 -0700

    define User class in models.py

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_ECHO'] = True #prints in ipython the queries being run
+
+app.config["SECRET_KEY"] = "maxcode1"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
 
 connect_db(app)
 db.create_all()
+
+#in ipython do db.session.execute('SELECt*from blah') and set to a vars and put in a list to see the data
--- models.py
+++ models.py
@@ -1,1 +1,24 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model): # usually only run this once after deployment otherwise you'll constantly be recreating data
+    """User."""
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, # int not the same as SQL Integer, the ORM translates etween python and postgreSQL
+                    primary_key=True,
+                    autoincrement=True)
+    
+    first_name = db.Column(db.String(50),
+                            nullable=False)
+
+    last_name = db.Column(db.String(50)) #maybe null as default?
+
+    image_url = db.Column(db.String(50)) #maybe add a default image
