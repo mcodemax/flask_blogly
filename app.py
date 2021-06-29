6eb5938e9cb987668b8fadbd512ec60e5de2f115
mcodemax <mwalterjohnson7@gmail.com>
2021-06-29 05:20:45 -0700

    add most html routes for app's purpose

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 """Blogly application."""
 
+import re
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
@@ -18,4 +19,86 @@ debug = DebugToolbarExtension(app)
 connect_db(app)
 db.create_all()
 
-#in ipython do db.session.execute('SELECt*from blah') and set to a vars and put in a list to see the data
+@app.route('/')
+def show_title_get():
+
+    #psql query the db and get a list of users
+
+
+    return redirect("/users")
+
+
+
+@app.route('/users')
+def list_users():
+
+    return render_template("userlist.html")
+#in ipython do db.session.execute('SELECt*from blah') and set to 
+# a vars and put in a list to see the data
+
+
+@app.route('/users/new')#method is get by default?
+def add_user_form():
+
+    return render_template('makeuser.html')
+
+    
+
+@app.route('/users/new', methods=["POST"])
+def post_user():
+
+    return redirect("/users")
+
+
+
+@app.route('/users/<int:user_num>', methods=["GET"])
+def show_question(user_num):
+    """Show information about the given user.
+
+    Have a button to get to their edit page, and to delete the user.
+    """
+
+
+    
+    return render_template('details.html',user_num=user_num)
+
+
+
+@app.route('/users/<int:user_num>/edit', methods=["GET"])
+def edit_user(user_num):
+    """
+    Show the edit page for a user.
+
+    Have a cancel button that returns to the detail page for 
+    a user, and a save button that updates the user.
+    """
+    # https://stackoverflow.com/questions/547821/two-submit-buttons-in-one-form
+
+    return render_template('edit.html',user_num=user_num)
+
+
+@app.route('/users/<int:user_num>/edit', methods=["POST"])
+def store_edits(user_num):
+    """
+    Process the edit form, returning the user to the /users page.
+    """
+
+    
+    return redirect('/users')
+
+
+
+@app.route('/users/<int:user_num>/delete', methods=["POST"])
+def delete_user(user_num):
+    """
+    Process the edit form, returning the user to the /users page.
+    """
+
+    # return the user to the /users page.
+    
+    #route not yet tested
+    return redirect('/users')
+
+
+
+# @app.route()
